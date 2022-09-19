b4722f16c779be6ec871adef233dd07c3d6a0d1a
FungusTrue <strindinlinus@gmail.com>
2022-09-19 20:46:11 +0200

    WordSearch

--- Program.cs
+++ Program.cs
@@ -4,9 +4,40 @@ namespace Morne_twitchbot2
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Counter to count cutes
+            int cuteCounter = 0;
+
+            while (true)
+            {
+                //test input
+                string test = Console.ReadLine();
+
+                //Check text for a word
+                WordSearch(test, "cute", ref cuteCounter);
+
+                //write out test 
+                Console.WriteLine(cuteCounter);
+            }
+
         }
+
+        static void WordSearch(string message, string checkWord, ref int checkWordCount)
+        {
+
+            Console.WriteLine(message);
+            string[] words = message.Split(" ");
+
+            foreach (var word in words)
+            {
+                if (word.ToLower() == checkWord)
+                {
+                    checkWordCount++;
+                }
+            }
+        }
+
     }
 }
